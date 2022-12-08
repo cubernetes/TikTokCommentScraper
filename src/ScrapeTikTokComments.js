5701111cf6de31ef0bf8c6e813a132f1a5589bd3
kaypineda <kpine003@odu.edu>
2022-12-08 09:53:07 -0500

    only save comments + basic info in csv file

--- src/ScrapeTikTokComments.js
+++ src/ScrapeTikTokComments.js
@@ -1,16 +1,14 @@
 
 with({
     copy
-}) {
+})
+{
     var commentsDivXPath                 = '//div[contains(@class, "DivCommentListContainer")]';
     var allCommentsXPath                 = '//div[contains(@class, "DivCommentContentContainer")]';
     var level2CommentsXPath              = '//div[contains(@class, "DivReplyContainer")]';
 
     var publisherProfileUrlXPath         = '//span[contains(@class, "SpanUniqueId")]';
     var nicknameAndTimePublishedAgoXPath = '//span[contains(@class, "SpanOtherInfos")]';
-
-    // we will filter these later because we have to handle them differently depending on what layout we have
-    var likesCommentsSharesXPath         = "//strong[contains(@class, 'StrongText')]";
 
     var postUrlXPath                     = '//div[contains(@class, "CopyLinkText")]'
     var descriptionXPath                 = '//h4[contains(@class, "H4Link")]/preceding-sibling::div'
@@ -24,25 +22,30 @@ with({
         let results = [];
         let query = document.evaluate(xpath, parent || document,
             null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-        for (let i = 0, length = query.snapshotLength; i < length; ++i) {
+        for (let i = 0, length = query.snapshotLength; i < length; ++i)
+        {
             results.push(query.snapshotItem(i));
         }
         return results;
     }
 
-    function getAllComments(){
+    function getAllComments()
+    {
         return getElementsByXPath(allCommentsXPath);
     }
 
-    function quoteString(s) {
+    function quoteString(s)
+    {
         return '"' + String(s).replaceAll('"', '""') + '"';
     }
 
-    function getNickname(comment) {
+    function getNickname(comment)
+    {
         return getElementsByXPath('./div[1]/a', comment)[0].outerText;
     }
 
-    function isReply(comment) {
+    function isReply(comment)
+    {
         return comment.parentElement.className.includes('Reply')
     }
 
@@ -64,30 +67,17 @@ with({
         }
     }
 
-    function extractNumericStats() {
-        var strongTags = getElementsByXPath(likesCommentsSharesXPath);
-        // the StrongText class is used on lots of things that aren't likes or comments; the last two or three are what we need
-		// if it's a direct URL, shares are displayed, so we want the last three; if not, we only want the last two
-        likesCommentsShares = parseInt(strongTags[(strongTags.length - 3)].outerText) ? strongTags.slice(-3) : strongTags.slice(-2);
-        return likesCommentsShares;
-    }
-
-    function csvFromComment(comment) {
-        nickname = getNickname(comment);
-        user = getElementsByXPath('./a', comment)[0]['href'].split('?')[0].split('/')[3].slice(1);
+    function csvFromComment(comment)
+    {
         commentText = getElementsByXPath('./div[1]/p', comment)[0].outerText;
-        timeCommentedAgo = formatDate(getElementsByXPath('./div[1]/p[2]/span', comment)[0].outerText);
-        commentLikesCount = getElementsByXPath('./div[2]', comment)[0].outerText;
-        pic = getElementsByXPath('./a/span/img', comment)[0] ? getElementsByXPath('./a/span/img', comment)[0]['src'] : "N/A";
-        return quoteString(nickname) + ',' + quoteString(user) + ',' + 'https://www.tiktok.com/@' + user + ','
-             + quoteString(commentText) + ',' + timeCommentedAgo + ',' + commentLikesCount;
+        return quoteString(commentText);
     }
 
     // Loading 1st level comments
     var loadingCommentsBuffer = 50; // increase buffer if loading comments takes long and the loop breaks too soon
     var numOfcommentsBeforeScroll = getAllComments().length;
-    while (loadingCommentsBuffer > 0) {
-
+    while (loadingCommentsBuffer > 0)
+    {
         allComments = getAllComments();
         lastComment = allComments[allComments.length - 1];
         lastComment.scrollIntoView(false);
@@ -95,9 +85,12 @@ with({
         numOfcommentsAftScroll = getAllComments().length;
 
         // If number of comments doesn't change after 15 iterations, break the loop.
-        if (numOfcommentsAftScroll !== numOfcommentsBeforeScroll) {
+        if (numOfcommentsAftScroll !== numOfcommentsBeforeScroll)
+        {
             loadingCommentsBuffer = 15;
-        } else {
+        }
+        else
+        {
             // direct URLs can get jammed up because there's a recommended videos list that sometimes scrolls first, so scroll the div just in case
             commentsDiv = getElementsByXPath(commentsDivXPath)[0];
             commentsDiv.scrollIntoView(false);
@@ -114,16 +107,21 @@ with({
 
     // Loading 2nd level comments
     loadingCommentsBuffer = 5; // increase buffer if loading comments takes long and the loop breaks too soon
-    while (loadingCommentsBuffer > 0) {
+    while (loadingCommentsBuffer > 0)
+    {
         readMoreDivs = getElementsByXPath(viewMoreDivXPath);
-        for (var i = 0; i < readMoreDivs.length; i++) {
+        for (var i = 0; i < readMoreDivs.length; i++)
+        {
             readMoreDivs[i].click();
         }
 
         await new Promise(r => setTimeout(r, 500));
-        if (readMoreDivs.length === 0) {
+        if (readMoreDivs.length === 0)
+        {
             loadingCommentsBuffer--;
-        } else {
+        }
+        else
+        {
             loadingCommentsBuffer = 5;
         }
         console.log('Buffer ' + loadingCommentsBuffer);
@@ -133,48 +131,29 @@ with({
 
     // Reading all comments, extracting and converting the data to csv
     var comments = getAllComments();
-    var level2CommentsLength = getElementsByXPath(level2CommentsXPath).length;
     var publisherProfileUrl = getElementsByXPath(publisherProfileUrlXPath)[0].outerText;
     var nicknameAndTimePublishedAgo = getElementsByXPath(nicknameAndTimePublishedAgoXPath)[0].outerText.replaceAll('\n', ' ').split(' · ');
 
     // direct URLs don't include a place to copy the link (since it'd be redundant) so just grab the actual page URL
     var url = window.location.href.split('?')[0]
-    var likesCommentsShares = extractNumericStats();
-    var likes = likesCommentsShares[0].outerText;
-    var totalComments = likesCommentsShares[1].outerText;
-
-    // the pop-up search window interface doesn't include shares
-    var shares = likesCommentsShares[2] ? likesCommentsShares[2].outerText : "N/A";
-    var commentNumberDifference = Math.abs(parseInt(totalComments) - (comments.length));
 
 
-    var csv = 'Now,' + Date() + '\n';
-    csv += 'Post URL,' + url + '\n';
+    var csv = 'Post URL,' + url + '\n';
     csv += 'Publisher Nickname,' + nicknameAndTimePublishedAgo[0] + '\n';
     csv += 'Publisher @,' + publisherProfileUrl + '\n';
-    csv += 'Publisher URL,' + "https://www.tiktok.com/@" + publisherProfileUrl + '\n';
-    csv += 'Publish Time,' + formatDate(nicknameAndTimePublishedAgo[1]) + '\n';
-    csv += 'Post Likes,' + likes + '\n';
-    csv += 'Post Shares,' + shares + '\n';
     csv += 'Description,' + quoteString(getElementsByXPath(descriptionXPath)[0].outerText) + '\n';
-    csv += 'Number of 1st level comments,' + (comments.length - level2CommentsLength) + '\n';
-    csv += 'Number of 2nd level comments,' + level2CommentsLength + '\n';
-    csv += '"Total Comments (actual, in this list, rendered in the comment section; needs all comments to be loaded!)",' + (comments.length) + '\n';
-    csv += "Total Comments (which TikTok tells you; it's too high most of the time when dealing with many comments OR way too low because TikTok limits the number of comments to prevent scraping)," + totalComments + '\n';
-    csv += "Difference," + commentNumberDifference + '\n';
-    csv += 'Comment Number (ID),Nickname,User @,User URL,Comment Text,Time,Likes,Is 2nd Level Comment,User Replied To,Number of Replies\n';
 
     var count = 1;
     var totalReplies = 0;
     var repliesSeen = 1; // Offset of replies from corresponding parent comment
     for (var i = 0; i < comments.length; i++) {
         csv += count + ',' + csvFromComment(comments[i]) + ',';
-        if (i > 0 && isReply(comments[i])) {
-            csv += "Yes," + quoteString(getNickname(comments[i - repliesSeen])) + ',0';
+        if (i > 0 && isReply(comments[i]))
+        {
             repliesSeen += 1;
         }
-        else {
-            csv += 'No,---,';
+        else
+        {
             totalReplies = 0;
             repliesSeen = 1;
             // Replies always come after the first comment, so look ahead until we reach another top-level comment
@@ -184,7 +163,6 @@ with({
                 }
                 totalReplies += 1;
             }
-            csv += totalReplies;
         }
         csv += '\n';
         count++;
